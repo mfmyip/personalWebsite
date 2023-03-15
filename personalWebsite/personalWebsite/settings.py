109af396d56935c3fd94fed16e478d71ca808353
Murphy <murphyyip49@gmail.com>
2023-03-15 00:10:38 -0400

    change DB to AWS posgresql

--- personalWebsite/personalWebsite/settings.py
+++ personalWebsite/personalWebsite/settings.py
@@ -12,25 +12,28 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# Get logins from env variable
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+POSTGRESQL_USER = os.getenv('POSTGRESQL_USER')
+POSTGRESQL_PASSWORD = os.getenv('POSTGRESQL_PASSWORD')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open( Path('personalWebsite') / 'secrets.json', 'r') as f:
-    data = json.load(f)
-    SECRET_KEY = data.get('SECRET_KEY')
-    POSTGRESQL_USER = data.get('POSTGRESQL').get('USER')
-    POSTGRESQL_PASSWORD = data.get('POSTGRESQL').get('PASSWORD')
+if not (SECRET_KEY and POSTGRESQL_USER and POSTGRESQL_PASSWORD):
+    with open( Path('personalWebsite') / 'secrets.json', 'r') as f:
+        data = json.load(f)
+        SECRET_KEY = data.get('SECRET_KEY')
+        POSTGRESQL_USER = data.get('POSTGRESQL').get('USER')
+        POSTGRESQL_PASSWORD = data.get('POSTGRESQL').get('PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['django-env.eba-qgudiyrj.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-app-dev3.us-east-2.elasticbeanstalk.com', '172.31.34.76', 'murphyyip.info']
 
 
 # Application definition
@@ -82,10 +85,10 @@ WSGI_APPLICATION = 'personalWebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_personalWebsite',
+        'NAME': 'ebdb',
         'USER': POSTGRESQL_USER,
         'PASSWORD': POSTGRESQL_PASSWORD,
-        'HOST': '127.0.0.1',
+        'HOST': 'awseb-e-8wkzi5t33e-stack-awsebrdsdatabase-bu0aeliajsq2.czffkad3dr3v.us-east-2.rds.amazonaws.com',
         'PORT': '5432'
     }
 }
