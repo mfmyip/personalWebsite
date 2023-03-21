2f21ead7b4b009bd73105d7362907b165db1bfee
Murphy <murphyyip49@gmail.com>
2023-03-20 21:35:51 -0400

    update templates

--- personalWebsite/projects/models.py
+++ personalWebsite/projects/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=50)
+    repo_link = models.CharField(max_length=100, default='https://github.com/mfmyip/')
     image = models.FilePathField(path="projects/static/img/")
     def __str__(self):
         return self.title
